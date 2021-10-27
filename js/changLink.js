534ca356276f569733ac5bf8c6d65061fc321866
ScriptKitt3h <jey1997@gmail.com>
2021-10-27 13:15:54 -0400

    updated 10-27-2021

--- js/changLink.js
+++ js/changLink.js
@@ -3,7 +3,14 @@ $('a').each(function(){
     var linkaddress = $(this).attr('href');
     console.log(linkaddress);
     $(this).attr('href',"https://www.eff.org");
-
+    
 });
 
 //update all image addresses
+$('img').each(function(){
+
+    var imgaddress = $(this).attr('src');
+    console.log(imgaddress);
+    $(this).attr('src',"https://cdn.discordapp.com/emojis/797285120900988928.png");
+
+});
